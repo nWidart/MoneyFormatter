8542988c6ed905647c79868884941b74606d447e
Nicolas Widart <n.widart@gmail.com>
2015-03-22 12:54:41 +0100

    Add basic tests for the formatting

--- tests/MoneyFormatterTest.php
+++ tests/MoneyFormatterTest.php
@@ -1,12 +1,28 @@
 <?php namespace Nwidart\MoneyFormatter\Tests;
 
+use Money\Currency;
+use Money\Money;
+use Nwidart\MoneyFormatter\MoneyFormatter;
+
 class MoneyFormatterTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * Test that true does in fact equal true
-     */
-    public function testTrueIsTrue()
+    /** @test */
+    public function should_format_as_english_pounds()
     {
-        $this->assertTrue(true);
+        $formatter = new MoneyFormatter('en_GB');
+        $this->assertEquals('£10.00', $formatter->format(new Money(1000, new Currency('GBP'))));
+    }
+
+    /** @test */
+    public function should_format_as_american_dollars()
+    {
+        $formatter = new MoneyFormatter('en_US');
+        $this->assertEquals('$10.00', $formatter->format(new Money(1000, new Currency('USD'))));
+    }
+
+    public function should_format_as_european_euros()
+    {
+        $formatter = new MoneyFormatter('fr_BE');
+        $this->assertEquals('10,00 €', $formatter->format(new Money(1000, new Currency('EUR'))));
     }
 }
